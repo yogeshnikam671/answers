03745d261389388d2e95554a619cf20b59ee0e09
Yogesh Nikam <Nikam_Yogesh@cat.com>
2020-09-30 15:22:22 +0530

    Ek ch commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './components/search/search.component';
+import { AnswerComponent } from './components/answer/answer.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    SearchComponent,
+    AnswerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+// @ts-ignore
+import questions from '../../data/questions.json';
+// @ts-ignore
+import answers from '../../data/answers.json';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SearchComponent implements OnInit {
+  question: string;
+  results: string[];
+  questions: string[];
+  answers: string[];
+  constructor() {
+    this.question = '';
+  }
+
+  ngOnInit(): void {
+    this.questions = questions.questions;
+    this.answers = answers.answers;
+  }
+
+  onClick(): void{
+    const ans = [];
+    this.questions.forEach((ques, index) => {
+      if (this.question && this.question !== ''
+            && ques.toLowerCase().includes(this.question.toLowerCase())){
+        ans.push(this.answers[index]);
+      }
+    });
+    this.results = ans;
+  }
+}
